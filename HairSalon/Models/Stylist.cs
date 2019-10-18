b6aa881cf6fd2a5aa54cbd6e3f42a2bbb3f61388
nikhilbatta <nbatta08@gmail.com>
2019-10-18 09:43:50 -0700

    adds stylist models

---
+++ HairSalon/Models/Stylist.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace HairSalon.Models
+{
+    public class Stylist
+    {
+        public int ID {get;set;}
+        public string Name {get;set;}
+        public string Specialties {get;set;}
+        public int Price {get;set;}
+    }
+}
