7ee779be773f1b69e821ef1686594cc31508a034
nikhilbatta <nbatta08@gmail.com>
2019-10-18 11:19:11 -0700

    adds virtual property of stylist to client, and adds collection of clients in stylist

--- HairSalon/Models/Client.cs
+++ HairSalon/Models/Client.cs
@@ -3,6 +3,8 @@ namespace HairSalon.Models
     public class Client
     {
         public int ClientID{get;set;}
+        public int StylistId {get;set;}
         public int ClientName {get;set;}
+        public virtual Stylist OneStylist {get;set;}
     }
 }
--- HairSalon/Models/Stylist.cs
+++ HairSalon/Models/Stylist.cs
@@ -5,9 +5,14 @@ namespace HairSalon.Models
 {
     public class Stylist
     {
-        public int ID {get;set;}
+        public int StylistId {get;set;}
         public string Name {get;set;}
         public string Specialties {get;set;}
         public int Price {get;set;}
+        public virtual ICollection<Client> clientCollect {get;set;}
+        public Stylist()
+        {
+            this.clientCollect = new HashSet<Client>{};
+        }
     }
 }
