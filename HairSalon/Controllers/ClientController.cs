9c8bbf889cbafb519501263620cb60ad14f11b8d
nikhilbatta <nbatta08@gmail.com>
2019-10-18 18:16:13 -0700

    forgot to save

--- HairSalon/Controllers/ClientController.cs
+++ HairSalon/Controllers/ClientController.cs
@@ -1,39 +1,46 @@
-// using HairSalon.Models;
-// using Microsoft.AspNetCore.Mvc;
-// using System.Linq;
-// using Microsoft.EntityFrameworkCore;
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
-// namespace HairSalon.Controllers
-// {
-//     public class ClientController: Controller
-//     {
-//          private readonly HairSalonContext _db;
-//         public ClientController(HairSalonContext db)
-//         {
-//             _db = db;
-//         }
-//         public ActionResult Index()
-//         {
+namespace HairSalon.Controllers
+{
+    public class ClientController: Controller
+    {
+         private readonly HairSalonContext _db;
+        public ClientController(HairSalonContext db)
+        {
+            _db = db;
+        }
+        public ActionResult Index()
+        {
+            List<Client> allClients = _db.Clients.ToList();
+            // should list out every client in the system, then should be able to click and find out their details. 
+            // carries an id to pass to details.
+            return View(allClients);
+        }
+        public ActionResult Details(int id)
+        {
             
-//             // should list out every client in the system, then should be able to click and find out their details. 
-//             // carries an id to pass to details.
-//             return View();
-//         }
-//         public ActionResult Details(int id)
-//         {
-//             // iteration  through colllection to find out if that id matches the id passed in and then returns that specific clients details.
-//             // action links on view page to redirect them to other parts of the website.
-//             return View();
-//         }
-//         public ActionResult Create(int id)
-//         {
-//             // i think i will have to pass in a viewbag or view data to also pass in the stylist "ids" or "names"
-//             // will show a form to create the new client
-//             return View();
-//         }
-//         // public ActionResult Create()
-//         // {
-//         //     // most likely will redirect them back to index
-//         // }
-//     }
-// }
+             
+            // iteration  through colllection to find out if that id matches the id passed in and then returns that specific clients details.
+            // action links on view page to redirect them to other parts of the website.
+            return View();
+        }
+        public ActionResult Create()
+        {
+            ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+            // has to pass in something to make the relationship between client and stylist.
+            // i think i will have to pass in a viewbag or view data to also pass in the stylist "ids" or "names"
+            // will show a form to create the new client
+            return View();
+        }
+        public ActionResult Create(Client newClient)
+        {
+            _db.Clients.Add(newClient);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+            // most likely will redirect them back to index
+        }
+    }
+}
